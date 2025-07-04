6979bf884ef17882ac0365aae4dd7d0af001c3e4
lcs-1 <lcsx1945@gmail.com>
2025-07-04 19:54:37 +0530

    Create GCD.java

---
+++ Mathematics/GCD.java
@@ -0,0 +1,20 @@
+//Euclid Theorem
+
+class Solution {
+    public static int gcd(int a, int b) {
+        // code here
+        while(a!=b){
+            if(a>b) a=a-b;
+            else b = b-a;
+        }
+        return a;
+    }
+}
+
+class Solution {
+    public static int gcd(int a, int b) {
+        // code here
+        if(b==0) return a;
+        return gcd(b,a%b);
+    }
+}
