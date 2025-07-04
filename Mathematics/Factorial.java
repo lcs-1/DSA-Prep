23a080acb039c4fe60bda6284acf08eaec933699
lcs-1 <lcsx1945@gmail.com>
2025-07-04 18:31:42 +0530

    Update Factorial.java

--- Mathematics/Factorial.java
+++ Mathematics/Factorial.java
@@ -9,3 +9,15 @@ class Solution {
         return res;
     }
 }
+class Solution {
+    static int factorial(int n) {
+        // code here
+        int res = 1;
+        for(int i=2;i<=n;i++){
+            res*=i;
+        }
+        return res;
+    }
+}
+
+
