0507880d4d2565211c6b20caf6bfba519f7bc4dc
lcs-1 <lcsx1945@gmail.com>
2025-07-06 14:25:02 +0530

    Update PrimeFactors.java

--- Mathematics/PrimeFactors.java
+++ Mathematics/PrimeFactors.java
@@ -16,3 +16,41 @@ class Solution {
         return arr;
     }
 }
+
+
+//More efficient
+
+class Solution {
+    public static ArrayList<Integer> primeFac(int n) {
+        // code here
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+        if(n<=1) return arr;
+        
+        while(n%2==0){
+            arr.add(2);
+            n = n/2;
+        }
+        while(n%3==0){
+            arr.add(3);
+            n = n/3;
+        }
+        
+        for(int i=5; i*i<=n; i+=6){
+            while(n%i==0){
+                arr.add(i);
+                n = n/i;
+            }
+            
+            while(n%(i+2)==0){
+                arr.add(i+2);
+                n = n/(i+2);
+            }
+        }
+        
+        if(n>3) arr.add(n);
+        
+        return arr;
+    }
+    
+}
